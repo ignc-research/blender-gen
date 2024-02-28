9e65cf0ddb16c2ab5ce176a75061e59b5cdcfc54
leoneversberg <leon.eversberg@tu-berlin.de>
2024-02-28 09:33:34 +0100

    small changes to show_annotations
    remove kps and set path according to config file

--- show_annotations.py
+++ show_annotations.py
@@ -6,37 +6,37 @@ import matplotlib.patches as patches
 import matplotlib.image as mpimg
 import random
 import json
+import sys
+import os
 
-with open('DATASET/Suzanne/annotations/instances_default.json') as f:
-  data = json.load(f)
+sys.path.append(os.getcwd())
+import config
 
-images = data['images']
-labels = data['annotations']
-while(True):
-    idx = random.randint(0,len(images)-1)
-    img_name = images[idx]['file_name']
-    print('showing annotation for img: ' + img_name)
-    print('close the window to see the next label')
+cfg = config.cfg()
 
-    bbox = labels[idx]['bbox']
+with open(f"DATASET/{cfg.out_folder}/annotations/instances_default.json") as f:
+    data = json.load(f)
 
-    I = mpimg.imread('DATASET/Suzanne/images/'+img_name)  # load rendered image
+images = data["images"]
+labels = data["annotations"]
+for idx in range(len(images)):
+    img_name = images[idx]["file_name"]
+    print("showing annotation for img: " + img_name)
+    print("close the window to see the next label")
 
-    fig,ax = plt.subplots(1)
+    bbox = labels[idx]["bbox"]
+
+    I = mpimg.imread(
+        f"DATASET/{cfg.out_folder}/images/" + img_name
+    )  # load rendered image
+
+    fig, ax = plt.subplots(1)
     ax.imshow(I)
-    plt.axis('off')
-    [x,y,w,h] = bbox
-    rect = patches.Rectangle((x,y),w,h,linewidth=2,edgecolor='g',facecolor='none')  # add bounding box annotation
+    plt.axis("off")
+    [x, y, w, h] = bbox
+    rect = patches.Rectangle(
+        (x, y), w, h, linewidth=2, edgecolor="g", facecolor="none"
+    )  # add bounding box annotation
     ax.add_patch(rect)
-    
-    vis_keypoints = False
-    if (vis_keypoints):
-      keypoints = labels[idx]['keypoints']
-      for kp in keypoints:
-        x = kp[0]
-        y = kp[1]
-        circle = patches.Circle((x,y), radius=3)
-        ax.add_patch(circle)
-    plt.show()
-    
 
+    plt.show()
